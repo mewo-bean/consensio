14c0263741e61938ac9a2eb6d2fcc18db7d7a809
Kin <kin234142@gmail.com>
2026-04-10 12:24:53 +0500

    Лого на смене пароля)

--- app/forgot/page.tsx
+++ app/forgot/page.tsx
@@ -3,6 +3,11 @@ import { ForgotForm } from "@/components/forgot-form"
 export default function ForgotPasswordPage() {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+            <a href="/" className="flex items-center gap-2 self-center font-medium">
+                <h1 className="text-5xl font-extrabold tracking-tight text-primary justify-end">
+                    consensio.
+                </h1>
+            </a>
             <ForgotForm />
         </div>
     )
